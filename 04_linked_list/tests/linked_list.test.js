2682848c2151e13be7cf878b82fdf4b8377ba89a
alexlhanson <alexlhanson@gmail.com>
2018-09-11 14:18:08 -0700

    Add npm init and dependancies

--- 04_linked_list/tests/linked_list.test.js
+++ 04_linked_list/tests/linked_list.test.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const LinkedList = require('./../lib/linked_list');
+
+describe('Linked-List constructor module', () => {
+
+  /********************************************************************************
+  *         linked-list class tests                                               *
+  ********************************************************************************/
+
+  test('should instantiate a linked-list with head value of null', () => {
+    let actual = new LinkedList();
+    let expected = null;
+    expect(actual.head).toBe(null);
+  })
+
+});
