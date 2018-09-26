3ee6c0ffc280c3d59a428a4c00ea8a4df79dff1f
alexlhanson <alexlhanson@gmail.com>
2018-09-26 16:08:49 -0700

    Add scaffolding and tests for tree constructor

--- 09_trees/__test__/trees.test.js
+++ 09_trees/__test__/trees.test.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const Tree = require('../lib/trees');
+const TreeNode = require('../lib/tree-node');
+
+describe('tree constructor', () => {
+
+  test('tree constructor returns object', () => {
+    let actual = new Tree();
+
+    expect(typeof actual).toBe('object');
+  })
+
+  test('tree constructor returns object with root and value', () => {
+    let node = new TreeNode(5);
+    let actual = new Tree(node);
+
+    expect(actual.root.value).toBe(5);
+  })
+});
--- 09_trees/lib/trees.js
+++ 09_trees/lib/trees.js
@@ -1,2 +1,81 @@
 'use strict';
 
+const TreeNode = require('../lib/tree-node');
+
+class Tree{
+
+  constructor(node){
+    this.root = node;
+  }
+
+  /********************************************************************************
+  *         Tree to Array and Traversal Patterns                                  *
+  ********************************************************************************/
+  //Create array from walk order root - left - right
+  preOrder(){
+    
+    let resultsArr = [];
+  
+    let _walk = node => {
+      results.push(node.value);
+      if(node.left) _walk(node.left);
+      if(node.left) _walk(node.left);
+    }
+
+    _walk(this.root);
+
+    return resultsArr
+  }
+  //Create array from walk order left - right - root
+  postOrder(){
+    
+    let resultsArr = [];
+  
+    let _walk = node => {
+      results.push(node.value);
+      if(node.left) _walk(node.left);
+      if(node.left) _walk(node.left);
+    }
+
+    _walk(this.root);
+
+    return resultsArr
+  }
+
+  //Create array from walk order left - root - right
+  inOrder(){
+    
+    let resultsArr = [];
+  
+    let _walk = node => {
+      results.push(node.value);
+      if(node.left) _walk(node.left);
+      if(node.left) _walk(node.left);
+    }
+
+    _walk(this.root);
+
+    return resultsArr
+  }
+
+  /********************************************************************************
+  *         BST  Methods                                                          *
+  ********************************************************************************/
+  //insert node method
+  insert(node){}
+
+  //remove node method
+  remove(node){}
+
+  //find node method
+  find(node){}
+
+  //serialize method
+  serialize(){}
+
+  //deserialize method
+  static deserialize(Tree){}
+
+}
+
+module.exports = Tree;
