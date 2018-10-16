aedf914abf0c1e0bee969a2cc07bb871d4fa9235
alexlhanson <alexlhanson@gmail.com>
2018-10-15 20:42:09 -0700

    Add graph and add methods/tests

--- 12_graphs/graph.test.js
+++ 12_graphs/graph.test.js
@@ -1,38 +1,5 @@
 'use strict';
 
-const Queue = require('../05_stacks_queues/lib/queue');
-
-class Graph {
-  constructor() {
-    this.vertices = {};
-  }
-
-  add(value, neighborsArr) {
-    if (!value) { throw new Error('Error: no vertex value provided') }
-    if (!neighborsArr) { throw new Error('Error: no neighbors array provided') }
-    
-    this.vertices[value] = neighborsArr;
-  }
-
-  breadthFirstTraversal(startVertex){
-    if (!startVertex) {throw new Error('no starting vertex provided')}
-    if (!this.vertices[startVertex]) {throw new Error('vertex not in graph')}
-
-    let traversalQueue = new Queue;
-    let visitedArr = [startVertex];
-
-    traversalQueue.enqueue(startVertex);
-    while(traversalQueue.size > 1){
-      
-    }
-  }
-}
-
-
-module.exports = Graph;
-
-'use strict';
-
 const Graph = require('./graph');
 
 describe('graph constructor', () => {
@@ -82,7 +49,5 @@ describe('graph add method', () => {
     let myGraph = new Graph();
     expect(() => {myGraph.add()}).toThrow('Error: no vertex value provided');
   })
-
-
 
 });
