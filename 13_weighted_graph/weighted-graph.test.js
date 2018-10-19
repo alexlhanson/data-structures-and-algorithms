ab868ac3bf66820c1fe04825b7378f5186ccb8a3
alexlhanson <alexlhanson@gmail.com>
2018-10-18 22:26:09 -0700

    Ad weighted graph addedge method

--- 13_weighted_graph/weighted-graph.test.js
+++ 13_weighted_graph/weighted-graph.test.js
@@ -23,7 +23,32 @@ describe('addVertex method', () => {
 
   test('should show error when no vertex is passed into function', () => {
     let myGraph = new WeightedGraph();
-    
-    expect(() => {myGraph.addVertex()}).toThrow('Error: no vertex parameter included');
+
+    expect(() => { myGraph.addVertex() }).toThrow('Error: no vertex parameter included');
+  })
+
+  test('should show that addVertex can be used with different primitive data types', () => {
+    let myGraph = new WeightedGraph();
+
+    myGraph.addVertex(5);
+    myGraph.addVertex(true);
+
+    expect(myGraph.adjList.get(5)).toBeDefined();
+    expect(myGraph.adjList.get(true)).toBeDefined();
+  })
+});
+
+describe('Add Edges Method', () => {
+
+  test('should show that edge is successfully added to both vertices', () => {
+    let myGraph = new WeightedGraph();    
+
+    myGraph.addVertex('Pandora');
+    myGraph.addVertex('Narnia');
+    myGraph.addEdge('Pandora', 'Narnia', 42);
+
+    expect(myGraph.adjList.get('Pandora').has('Narnia'))
+    expect(myGraph.adjList.get('Narnia').has('Pandora'))
+
   })
 });
